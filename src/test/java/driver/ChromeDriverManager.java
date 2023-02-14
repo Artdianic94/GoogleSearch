fdd309770dbd85f343d0b7b73bd43eee07166843
ArtDiaNic <Artdianic@gmail.com>
2023-02-14 09:12:44 +0300

    selenoid was added

--- src/test/java/driver/ChromeDriverManager.java
+++ src/test/java/driver/ChromeDriverManager.java
@@ -1,25 +1,56 @@
 package driver;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChromeDriverManager extends DriverManager {
 
 
     @Override
-    public void setUpDriver() {
-        WebDriverManager.chromedriver().setup();
+    public void setUpDriver() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        System.setProperty("webdriver.chrome.args", "--disable-logging");
-        System.setProperty("webdriver.chrome.silentOutput", "true");
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("window-size=1024,768"); // Bypass OS security model
-        driver = new ChromeDriver(options);
+
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            /* How to add test badge */
+            put("name", "Google test");
+
+            /* How to set session timeout */
+            put("sessionTimeout", "15m");
+
+            /* How to set timezone */
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+
+            /* How to add "trash" button */
+            put("labels", new HashMap<String, Object>() {{
+                put("manual", "true");
+            }});
+
+            /* How to enable video recording */
+            put("enableVideo", true);
+        }});
+        // RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+//        WebDriverManager.chromedriver().setup();
+        //ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        System.setProperty("webdriver.chrome.args", "--disable-logging");
+//        System.setProperty("webdriver.chrome.silentOutput", "true");
+//        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//        options.addArguments("disable-infobars"); // disabling infobars
+//        options.addArguments("--disable-extensions"); // disabling extensions
+//        options.addArguments("--disable-gpu"); // applicable to windows os only
+//        options.addArguments("window-size=1024,768"); // Bypass OS security model
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
     }
 }
